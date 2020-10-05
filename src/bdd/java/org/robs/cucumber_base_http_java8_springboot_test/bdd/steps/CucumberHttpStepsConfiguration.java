4c963739856e35ac8643b0c40af5fec2be6c9a96
Robert Baboi <robert.baboi@gmail.com>
2020-10-05 21:05:18 +0100

    Updated for changes in 0.1.1-SNAPSHOT

--- src/bdd/java/org/robs/cucumber_base_http_java8_springboot_test/bdd/steps/CucumberHttpStepsConfiguration.java
+++ src/bdd/java/org/robs/cucumber_base_http_java8_springboot_test/bdd/steps/CucumberHttpStepsConfiguration.java
@@ -2,8 +2,13 @@ package org.robs.cucumber_base_http_java8_springboot_test.bdd.steps;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.spring.CucumberContextConfiguration;
-import io.github.brobert83.cucumber_http_java8.request_handlers.unirest.CucumberBaseSpringConfig;
+import io.github.brobert83.cucumber_http_java8.request_handlers.HttpRequestHandler;
+import io.github.brobert83.cucumber_http_java8.CucumberHttpSpringConfig;
+import io.github.brobert83.cucumber_http_java8.request_handlers.unirest.common.SendUnirestRequestWithBody;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
 import org.robs.cucumber_base_http_java8_springboot_test.CucumberBaseHttpJava8SpringBootTestApplication;
+import org.slf4j.Logger;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.annotation.Bean;
@@ -12,26 +17,31 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Map;
 import java.util.function.Supplier;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 @ActiveProfiles("cucumber")
 @CucumberContextConfiguration
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         classes = {
-                StepsBase.SpringTestConfig.class,
-                CucumberBaseSpringConfig.class,
+                CucumberHttpStepsConfiguration.SpringTestConfig.class,
+                CucumberHttpSpringConfig.class,
                 CucumberBaseHttpJava8SpringBootTestApplication.class
         }
 )
-public class StepsBase {
+public class CucumberHttpStepsConfiguration {
+
+    private static final Logger logger = getLogger(CucumberHttpStepsConfiguration.class);
 
     @Lazy
     @Profile("cucumber")
     @Configuration
     public static class SpringTestConfig {
 
-        // ==== REQUIRED BY HttpSteps ====
+        // ==== Customizations ====
         @Bean
         Supplier<ObjectMapper> objectMapperSupplier(ObjectMapper objectMapper) {
             return () -> objectMapper;
@@ -43,6 +53,14 @@ public class StepsBase {
             return () -> "http://localhost:" + port;
         }
 
+        @Bean
+        boolean customPostHandler(Map<String, HttpRequestHandler<HttpResponse<String>>> unirestHttpHandlers, SendUnirestRequestWithBody sendUnirestRequestWithBody) {
+            unirestHttpHandlers.put("post", context -> {
+                logger.info("Sending custom post request");
+                return sendUnirestRequestWithBody.apply(Unirest.post(context.getUrl()), context);
+            });
+            return true;
+        }
     }
 
 }
